d00147cf3718a273a9914321fa5fcbb4f28177b2
Douglas Hall <dhall@asusmo.(none)>
2011-05-05 18:12:01 -0700

    image gallery in infobubble

---
+++ js/markerjson.js
@@ -0,0 +1,276 @@
+var markerjson =
+{
+    "backgroundColor" : "1",
+    "markers" :
+    [
+        {
+            "id" : "trulloveMarker",
+            "imageFile" : "images/trullove-marker.png",
+            "lat" : 40.624580,
+            "lng" : 17.40206,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Trullove",
+                    "divText" : "<h4>For Rent: Magnificent Trulli Villa</h4><p>Trullove is a magnificently restored trullo complex surrounded by 20 acres of quiet, sunbaked olive groves and fruit orchards. Ideal for families and larger groups of friends, the property sleeps up to 16 people giving all members of the family or group their own space. Lovely shaded drystone walled travertine terraces and patios with padded stone seats, sun loungers and umbrellas transform the garden and spacious infinity-edge pool into splendid open-air lounges, creating an enchanting place to relax.</p>" ,
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Region",
+                    "divText" : "<h4>Puglia: Quintessentially Italian</h4><p>Trullove is a centrally located in Puglia, or Apulia as it is also known, an undiscovered region in Southern Italy in the heel of Italy's boot. Puglia is flanked by the Adriatic and Ionian Seas and is rich in heritage and culture with picturesque historical towns, castles, churches and architectural masterpieces, breathtaking scenery, beautiful coastlines, local markets, castles, historical sites, traditional food, wines and olive oils. It is a most ravishing distillation of everything that admiring visitors to Puglia consider quintessentially Italian.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Directions",
+                    "divText" : "<p>Puglia is serviced by two main airports, Bari Airport and Brindisi Airport.  Ryanair flies daily to both, from London Stansted, all year round. British Airways flies from Gatwick to Bari twice a week from April to October.  Ryanair flies from Brussels Charleroi to Brindisi three times a week, all year round and to Bari three times a week, from April till October. Ryanair also flies from Eindhoven Airport to Brindisi twice a week, from March till October and to Bari. Trullove is located in the hills near Martina Franca.  It's a 50 minute drive from Brindisi Airport and an hour and a half from Bari Airport. Detailed directions will be provided upon confirmation of booking." +
+                        "<br /><br /><a target=\"_blank\" href=\"http://maps.google.com/maps?f=d&source=s_d&saddr=Brindisi+Airport,+Brindisi,+Italy&daddr=Strada+Trazzonara&hl=en&geocode=FT52bAIdOsgRASFLo0xBL23pZg%3BFUrjawIdPIcJAQ&mra=dme&mrsp=1&sz=16&sll=40.6248,17.403996&sspn=0.008159,0.011137&ie=UTF8&ll=40.61291,17.637177&spn=0.533759,0.881653&z=10\">Driving Directions from Brinidisi Airport</a>" +
+                        "<br /><br /><a target=\"_blank\" href=\"http://maps.google.com/maps?f=d&source=s_d&saddr=Bari+Airport,+Bari,+Italy&daddr=Strada+Trazzonara&geocode=FWS1cwIdEtH_ACECG_DmOKe60A%3BFUrjawIdPIcJAQ&hl=en&mra=ls&sll=40.880295,17.148285&sspn=0.531617,0.881653&ie=UTF8&ll=40.813809,17.306213&spn=1.105873,1.925354&z=9\">Driving Directions from Bari Airport</a></p>",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "airportBrindisiMarker",
+            "imageFile" : "images/airport-icon.png",
+            "lat" : 40.65824,
+            "lng" : 17.93921,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Brindisi Airport",
+                    "divText" : "<p>Ryanair flies daily to Brindisi Airport, from London Stansted, all year round and from Brussels Charleroi three times a week, all year round. Ryanair also flies from Eindhoven Airport to Brindisi twice a week, from March till October. Trullove is located in the hills near Martina Franca.  It's a 50 minute drive from Brindisi Airport. Detailed directions will be provided upon confirmation of booking." +
+                        "<br /><br /><a href=\"http://www.aeroportidipuglia.it/default.asp?idlingua=2&idcontenuto=29\" target=\"_blank\">Brindisi Airport Website</a>" +
+                        "<br /><br /><a target=\"_blank\" href=\"http://maps.google.com/maps?f=d&source=s_d&saddr=Brindisi+Airport,+Brindisi,+Italy&daddr=Strada+Trazzonara&hl=en&geocode=FT52bAIdOsgRASFLo0xBL23pZg%3BFUrjawIdPIcJAQ&mra=dme&mrsp=1&sz=16&sll=40.6248,17.403996&sspn=0.008159,0.011137&ie=UTF8&ll=40.61291,17.637177&spn=0.533759,0.881653&z=10\">Google Driving Directions from Brindisi Airport</a></p>",
+                    "image" : "images/brindisi_airport.jpg"
+                }
+                /*,
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "<a target=\"_blank\" href=\"http://maps.google.com/maps?f=d&source=s_d&saddr=Brindisi+Airport,+Brindisi,+Italy&daddr=Strada+Trazzonara&hl=en&geocode=FT52bAIdOsgRASFLo0xBL23pZg%3BFUrjawIdPIcJAQ&mra=dme&mrsp=1&sz=16&sll=40.6248,17.403996&sspn=0.008159,0.011137&ie=UTF8&ll=40.61291,17.637177&spn=0.533759,0.881653&z=10\">Driving Directions</a>",
+                    "image" : ""
+                } */
+            ]
+        },
+        {
+            "id" : "airportBariMarker",
+            "imageFile" : "images/airport-icon.png",
+            "lat" : 41.13762,
+            "lng" : 16.76522,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Bari Airport",
+                    "divText" : "<p>Ryanair flies daily to Bari Airport, from London Stansted, all year round. British Airways flies from Gatwick twice a week from April to October. Ryanair flies from Brussels Charleroi to Bari three times a week from April till October.  Lufthansa flies daily from Munich as well. Trullove is located in the hills near Martina Franca.  It's a an hour and a half drive from Bari Airport. Detailed directions will be provided upon confirmation of booking." +
+                        "<br /><br /><a target=\"_blank\" href=\"http://www.aeroportidipuglia.it/default.asp?idlingua=2&idcontenuto=25\">Bari Airport Website</a>" +
+                        "<br /><br /><a target=\"_blank\" href=\"http://maps.google.com/maps?f=d&source=s_d&saddr=Bari+Airport,+Bari,+Italy&daddr=Strada+Trazzonara&geocode=FWS1cwIdEtH_ACECG_DmOKe60A%3BFUrjawIdPIcJAQ&hl=en&mra=ls&sll=40.880295,17.148285&sspn=0.531617,0.881653&ie=UTF8&ll=40.813809,17.306213&spn=1.105873,1.925354&z=9\">Google Driving Directions from Bari Airport</a></p>",
+                    "image" : "images/bari_airport.jpg"
+                }
+                /*,
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "<a target=\"_blank\" href=\"http://maps.google.com/maps?f=d&source=s_d&saddr=Bari+Airport,+Bari,+Italy&daddr=Strada+Trazzonara&geocode=FWS1cwIdEtH_ACECG_DmOKe60A%3BFUrjawIdPIcJAQ&hl=en&mra=ls&sll=40.880295,17.148285&sspn=0.531617,0.881653&ie=UTF8&ll=40.813809,17.306213&spn=1.105873,1.925354&z=9\">Google Driving Directions from Bari Airport</a>",
+                    "image" : ""
+                } */
+            ]
+        },
+        {
+            "id" : "locorotondoMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.7554,
+            "lng" : 17.3263,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Locorotondo",
+                    "divText" : "<p>Locorotondo is quite magical!  Locorotondo is just a 15 minute drive from the trullo and the centre is a lovely place for a stroll. It has shiny clean limestone paved streets and flower filled balconies. In my book it would definitely get the prize for the best kept village!</p>",
+                    "image" : "images/trullove-puglia-trulli-locorotondo-1.png"
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p>Friday: Locorotondo:- A small but pretty market.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "lecceMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.3528,
+            "lng" : 18.174,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Lecce",
+                    "divText" : "<p></p>" +
+                        "<div class=\"slideshow\">" +
+                        "<img src=\"http://cloud.github.com/downloads/malsup/cycle/beach1.jpg\" width=\"200\" height=\"200\" /> " +
+                        "<img src=\"http://cloud.github.com/downloads/malsup/cycle/beach2.jpg\" width=\"200\" height=\"200\" /> " +
+                        "<img src=\"http://cloud.github.com/downloads/malsup/cycle/beach3.jpg\" width=\"200\" height=\"200\" /> " +
+                        "<img src=\"http://cloud.github.com/downloads/malsup/cycle/beach4.jpg\" width=\"200\" height=\"200\" /> " +
+                        "<img src=\"http://cloud.github.com/downloads/malsup/cycle/beach5.jpg\" width=\"200\" height=\"200\" /> " +
+                        "</div> ",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p></p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "martinaFrancaMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.7041,
+            "lng" : 17.3349,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Martina Franca",
+                    "divText" : "<p>Martina Franca has one of the finest old quarters in Puglia.  Baroque balconies decorated with ornate wrought iron look down on narrow alleyways that have hardly changed in centuries.  In the second  half of July and early August, the town stages the annual Festival della Vale d'Itria - a feast for opera lovers. Martina Franca is a town with a history that makes its wealth and fine architecture something not to be missed. There are many fine shops here that make an early evening 'passeggiata' particularly interesting.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p>Wednesday: Martina Franca, nowadays much of the town's people are employed in clothing or furniture production which means that a visit to the Wednesday morning market is a good idea and you will certainly find a bargain.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "OstuniMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.728657,
+            "lng" : 17.571301,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Ostuni",
+                    "divText" : "<p>Ostuni, known as the White City because of the proliferation of white-washed stone buildings, resembles a North-African Arab Medina due to the seemingly chaotic tangle of narrow cobblestone streets.  Rising above it all in sombre brown stone is the beautiful 15th century Gothic cathedral.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p>Saturday: Ostuni, a big market on the outskirts of Ostuni, head for the Francavilla road around Ostuni and the market is on your left. It is bigger than it seems and you will need an early start in hot weather to get through it all. Look out for shoes, bags, accessories, Italian crafts and of course fruit and vegetables that are in season.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "CeglieMessapicaMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.785578,
+            "lng" : 17.238579,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Ceglie Messapica",
+                    "divText" : "<p>Ceglie Messapica can be entered via two doors: Giuso and Monterone. From here, through a sinuous path, one will arrive at the climb up to the 'Centro Scaloni,' existing since Messapian times and updated during the Medieval period. It leads to the Castle area. The village centre is dominated by the presence of the Castle, initiated during Norman times (11th Century AD). Ceglie Messapica hasn't the Disney qualities of a postcard perfect place that has been gussied up for tourists. It is more authentic (meaning there are more locals than tourists) and has more 'local' feel than lovely towns like Ostuni and Martina Franca.  Local people cherish the character of this medieval town, yet there's not a mono-economy of tourism. Street architecture, traffic, people and day-to-day life seem a bit contradictory, random, bohemian and somewhat zany.  That's what we like about Ceglie Messapica.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p></p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "CisterninoMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.743153,
+            "lng" : 17.425058,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Cisternino",
+                    "divText" : "<p>Cisternino is a well kept secret. Think of an Italian hill top town and Cisternino fits that description. Even the Italians think it special and have listed it as One of the Best Small Towns in Italy. 'Uno dei Borghi piu Belli D'Italia' says the sign outside the town. This is certainly because of the position and the architecture, the food, the people, what's on all year round and the happy combination of all these. In summer a good idea is to visit it in the evening, as all the shops in the old town are open, lots of people are around and you can sit in the local \"piazza\" and listen to music.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p>Monday: Cisternino, for fruit and vegetables, clothes, shoes and household neccessities. Smallish with nearby bars which makes it a bearable morning with young children in tow!</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "locorotondoMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.7554,
+            "lng" : 17.3263,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Locorotondo",
+                    "divText" : "<p>Locorotondo is quite magical!  Locorotondo is just a 15 minute drive from the trullo and the centre is a lovely place for a stroll. It has shiny clean limestone paved streets and flower filled balconies. In my book it would definitely get the prize for the best kept village!</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p>Friday: Locorotondo:- A small but pretty market.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        },
+        {
+            "id" : "GrottaglieMarker",
+            "imageFile" : "images/pushpin.png",
+            "lat" : 40.537743,
+            "lng" : 17.435678,
+            "tabs" :
+            [
+                {
+                    "tabText" : "Grottaglie",
+                    "divText" : "<p>The hub of ceramics production is Grottaglie. In the heart of this characteristic city, behind the medieval castle, lies a centuries-old quarter of ceramicists who developed their trade by following age-old working methods.  There are two main products of the Grottaglie figurine tradition: the \"Bianchi di Grottaglie\" or \"white\" artistic production that exalts pure form through the use of white stanniferous glaze with no other decoration and typical of a more elite production, and the more characteristic rustic and popular ceramics flaunting a palette of colours that ranges from green, ochre, blue, and manganese.</p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Markets",
+                    "divText" : "<p></p>",
+                    "image" : ""
+                },
+                {
+                    "tabText" : "Driving Directions",
+                    "divText" : "",
+                    "image" : ""
+                }
+            ]
+        }
+    ]
+}
