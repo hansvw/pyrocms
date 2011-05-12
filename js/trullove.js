fbb5d7504d51269421a05992ec59c98dc076d653
Hans Van Wesenbeeck <hansvw@aivolution.com>
2011-05-12 08:25:20 -0700

    minwidth / maxwidth IE7-9

--- js/trullove.js
+++ js/trullove.js
@@ -90,7 +90,8 @@
         var infoBubble = new InfoBubble(
         {
             shadowStyle: 1,
-            minWidth: 500
+            minWidth: 600,
+            maxWidth: 600
         });
 
 //        google.maps.event.addListener(infoBubble, 'domready', function()
